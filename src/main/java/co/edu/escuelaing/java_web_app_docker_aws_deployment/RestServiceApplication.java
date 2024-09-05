360632c8976103382b3952ee7e05396cffea8cc6
JohannBulls <amayapez99@gmail.com>
2024-09-05 13:27:28 -0500

    Inicializar

---
+++ src/main/java/co/edu/escuelaing/java_web_app_docker_aws_deployment/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.java_web_app_docker_aws_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class RestServiceApplication {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+ 
+}
---
+++ src/test/java/co/edu/escuelaing/java_web_app_docker_aws_deployment/JavaWebAppDockerAwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.java_web_app_docker_aws_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebAppDockerAwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
